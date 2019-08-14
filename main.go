1fae997e9fd0ccb1f902d75899f4cca111ace114
ashokshingade24 <ashokshingade24@gmail.com>
2019-08-14 07:46:41 +0530

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello Ashok!!"}
+		res := &response{Message: "Hello K8s!!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
